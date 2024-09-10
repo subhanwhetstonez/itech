7266f83f604371a70a163e627ab3432319734eb8
Subhan <subhan.suleman@whetstonez.com>
2024-09-10 13:54:06 +0500

    Portfolio completed

--- app/portfolio/page.jsx
+++ app/portfolio/page.jsx
@@ -4,23 +4,19 @@ import Logocomp from "../content/logo";
 
 const page = () => {
   return (
-    <div className="relative z-10">
-      <div className="grid grid-cols-3 grid-rows-4 object-contain">
-        <img src="gal1.jpg" alt="" className="col-start-1 row-start-1" />
-        <img src="gal2.jpg" alt="" className="col-start-2 row-start-1" />
-        <img src="gal8.jpg" alt="" className="col-start-3 row-span-2" />
-        <img src="gal4.jpg" alt="" className="col-start-1 row-start-2" />
-        <div className="bg-[#6953D3] grid col-start-2 row-start-2 row-span-2">
-          <img src="itechwhite.png" alt="" className="place-self-center" />
+    <div className="relative z-10 bg-white p-5 md:p-10 lg:px-48">
+      <div className="column-1 md:columns-3 gap-5 md:gap-8 [&>img:not(:first-child)]:mt-5 md:[&>img:not(:first-child):mt-8]">
+        <img src="gal1.jpg" alt="" />
+        <img src="gal2.jpg" alt="" />
+        <img src="gal6.jpg" alt="" />
+        <img src="gal3.jpg" alt="" />
+        <div className="bg-[#6953D3] h-[80vh] mt-4 grid place-content-center">
+          <img src="itechwhite.png" alt="" />
         </div>
         <img src="gal5.jpg" alt="" />
+        <img src="gal8.jpg" alt="" />
         <img src="gal7.jpg" alt="" />
-        <img
-          src="gal6.jpg"
-          alt=""
-          className="col-start-1 row-start-3 row-span-2"
-        />
-        <img src="gal3.jpg" alt="" />
+        <img src="gal4.jpg" alt="" />
       </div>
     </div>
   );
